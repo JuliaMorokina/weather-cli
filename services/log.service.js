1d8288d1b7ec7eeef742142a2598ceac5fb32869
Julia Morokina <juliamorokina@mail.ru>
2022-09-13 00:21:46 +0300

    Работа с файловой системой [new][S] - запись значения в файл, чтение файла
    [new][S] - сохранение токена, вывод сообщений о результате сохранения

--- services/log.service.js
+++ services/log.service.js
@@ -2,18 +2,18 @@ import chalk from "chalk";
 import dedent from "dedent-js";
 
 const printError = (err) => {
-  const errorMessage = `${chalk.bgRed("ERROR")}: ${err}`;
+  const errorMessage = `${chalk.bgRed(" ERROR ")} ${err}`;
   console.log(errorMessage);
 };
 
 const printSuccess = (message) => {
-  const successMessage = `${chalk.bgGreen("SUCCESS")}: ${message}`;
+  const successMessage = `${chalk.bgGreen(" SUCCESS ")} ${message}`;
   console.log(successMessage);
 };
 
 const printHelp = () => {
   const helpMessage = dedent`
-    ${chalk.bgCyan("HELP")} 
+    ${chalk.bgCyan(" HELP ")} 
     Без параметров - вывод погоды
     -s [CITY] - установка города
     -t [API_KEY] - сохранение токена

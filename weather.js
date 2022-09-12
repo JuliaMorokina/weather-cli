e64fbc7951a91089dcc003cda58eed50ce152f3e
Julia Morokina <juliamorokina@mail.ru>
2022-09-12 22:44:23 +0300

    Работа с аргументами [new][S] - добавлен хелпер для разбора переданных аргументов

--- weather.js
+++ weather.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+
+  if(args.s) {
+    // save city
+  } else if(args.h) {
+    // help
+  } else if(args.t) {
+    // save token
+  }
+
+  // weather show
+};
+
+initCLI();

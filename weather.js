b333b76a4e03ad379a8a4855855732d20942a8c3
Julia Morokina <juliamorokina@mail.ru>
2022-09-12 23:33:24 +0300

    Storage service [new][S] - создан метод для сохранения значений в локальный файл пользователя

--- weather.js
+++ weather.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
 
   if (args.s) {
-    // save city
+    saveKeyValue("city", args.s);
   } else if (args.h) {
     printHelp();
   } else if (args.t) {
-    // save token
+    saveKeyValue("token", args.t);
   }
 
   // weather show

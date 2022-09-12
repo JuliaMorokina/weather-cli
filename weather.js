78ecddc94078856158962990c3799b7567d08693
Julia Morokina <juliamorokina@mail.ru>
2022-09-12 23:14:00 +0300

    Log service [new][S] - создан log service для вывода сообщений в консоль

--- weather.js
+++ weather.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printHelp } from "./services/log.service.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
 
-  if(args.s) {
+  if (args.s) {
     // save city
-  } else if(args.h) {
-    // help
-  } else if(args.t) {
+  } else if (args.h) {
+    printHelp();
+  } else if (args.t) {
     // save token
   }
 
